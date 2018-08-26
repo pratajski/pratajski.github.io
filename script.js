dece5f26ffa3681661038d9ada9c76b9a1b7b568
pratajski <p.ratajski@gmail.com>
2018-08-26 13:36:23 +0200

    fronted upgrade cd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://tranquil-stream-76187.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://tranquil-stream-76187.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
